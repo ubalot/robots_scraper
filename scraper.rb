258629170c7604732a9289b5304c0eb79d569ca0
Luca <ubalot1@gmail.com>
2017-10-19 01:38:40 +0200

    Add scraper.rb

---
+++ scraper.rb
@@ -0,0 +1,78 @@
+require 'open-uri'
+
+
+websites = [
+  'google.com', 'amazon.com', 'paypal.com', 'pgdsjk.gprejg'
+]
+
+headers = ['https', 'http']
+
+websites_urls = websites.map do |website|
+  { website => headers.map { |header| "#{header}://#{website}" } }
+end
+
+
+class Website
+  def initialize(website_url)
+    @robot_url = website_url
+    @robot_content = getTargetContent
+  end
+
+  def show
+    puts "@robot_url #{@robot_url}"
+    puts "@robot_content #{@robot_content}"
+  end
+
+  private
+
+  def getTargetContent
+    begin
+        content = open(@robot_url).read unless @robot_url.nil?
+        content unless content.nil? || !content['User-agent']
+    rescue Errno::ECONNREFUSED => error
+        nil
+    end
+  end
+end
+
+
+class RobotScraper
+  def initialize(websites_urls)
+    @websites_urls = websites_urls
+    tmp = valid_urls
+    @targets = tmp.map do |target|
+      target.map { |website, url|  Website.new(url) unless url.nil? }
+    end
+  end
+
+  def show
+    puts "websties_urls #{@websites_urls}"
+    @targets.map do |target| target.map { |t| t.show } end
+    puts
+  end
+
+  private
+
+  def valid_urls
+    @websites_urls.map do |target|
+      target.map { |website, urls|
+        urls.map { |url|
+          robot_url = "#{url}/robots.txt"
+          { website => robot_url } unless (validator robot_url).nil?
+        }.compact.first
+      }.compact.first
+    end
+  end
+
+  def validator(robot_url)
+    begin
+        !open(robot_url).nil?
+    rescue Errno::ECONNREFUSED => error
+        nil
+    end
+  end
+
+end
+
+rs = RobotScraper.new(websites_urls)
+rs.show

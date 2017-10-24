36d07e1f7cb1e089db53aaeea46437dd6e98adea
Luca Albonetti <alboyoyo@gmail.com>
2017-10-25 00:00:04 +0200

    bugfix

--- scraper.rb
+++ scraper.rb
@@ -7,12 +7,16 @@ DEBUG = true
 
 ROBOT_LIST_FILE = "robots_list.txt"
 
+websites = []
+File.open(ROBOT_LIST_FILE, "r") do |f|
+  f.each_line do |line|
+    line["\n"] = ""  # avoid '\n' at the end of the string
+    websites.push(line) unless line.empty?
+  end
+end
+websites = websites.uniq
 
-websites = [
-  'google.com', 'amazon.com', 'paypal.com', 'pgdsjk.gprejg'
-].uniq
-websites = ['google.com'] if DEBUG
-
+# websites = ['google.com'] if DEBUG
 
 headers = ['https', 'http']
 
@@ -25,20 +29,23 @@ websites_urls = Hash[websites.map { |website|
 class RobotsScraper
 
   def initialize(websites_urls)
-    puts "websites_urls #{websites_urls}" if DEBUG
+    # puts "websites_urls #{websites_urls}" if DEBUG
     @targets = Hash[websites_urls.map { |website, urls|
+      puts "URL #{urls}"
       url = urls.map { |url|
-        robots_url = "#{url}/robots.txt"
+        robots_url = url + "/robots.txt"#{}"#{url}/robots.txt"
         robots_url if exists robots_url
       }.first
       [ website, url ]
-    }].delete_if { |website, url| url.nil? }.map { |website, url|
+     }].delete_if { |website, url| url.nil? }.map { |website, url|
+      puts "website #{website}"
+      puts "url #{url}"
       [website, RobotsAnalyzer.new(url)]
     }
   end
 
   def show
-    # puts "targets #{@targets}"
+    puts "targets #{@targets}"
     @targets.map { |website, robotsAnalyzer| robotsAnalyzer.show }
     puts
   end
@@ -47,7 +54,6 @@ class RobotsScraper
 
   def exists(robots_url)
     begin
-        puts "robot_url #{robots_url}" if DEBUG
         true unless open(robots_url).nil? or (open(robots_url).read)['User-agent'].nil?
     rescue Exception
         nil
